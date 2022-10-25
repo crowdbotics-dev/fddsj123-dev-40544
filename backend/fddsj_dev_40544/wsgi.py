d7b28847b032341b35d02c00857bdee3fa345804
Crowdbotics <engineering@crowdbotics.com>
2022-10-25 06:15:05 +0000

    Initial commit.

---
+++ backend/fddsj_dev_40544/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fddsj_dev_40544 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fddsj_dev_40544.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fddsj_dev_40544",
+  url: "https://fddsj_dev_40544.botics.co",
+  api: "https://fddsj_dev_40544.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
